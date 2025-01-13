37aff723650e7446db1cdc535fee7ea38535d792
CitymineHD <lucas.pfeffer@gmx.de>
2025-01-13 10:04:15 +0100

    changed network arrays v2

--- network.c
+++ network.c
@@ -11,18 +11,16 @@
 
 #define Number_of_Layer 3 //Input Layer + Hidden Layer + Output Layer
 
-double network_weights_input[Number_of_Layer-2][Number_of_Hidden_Neurons][Number_of_Hidden_Neurons];
+double network_weights_input[Number_of_Hidden_Neurons][Number_of_Input_Neurons];
 double network_weights_output[Number_of_Output_Neurons][Number_of_Output_Neurons];
 double threshold[Number_of_Layer-1][Number_of_Output_Neurons];
 
 void initial_network_weights() {
     srand((unsigned int)time(NULL));
-    for (int i = 0; i < (Number_of_Layer-1); i++) {
-        for (int n = 0; n < Number_of_Hidden_Neurons; n++) {
-                for (int x = 0; x < Number_of_Hidden_Neurons; x++) {
-                    network_weights_input[i][n][x] = (float)rand() / RAND_MAX * 0.001;
-                }
-        }
+    for (int n = 0; n < Number_of_Hidden_Neurons; n++) {
+            for (int x = 0; x < Number_of_Input_Neurons; x++) {
+                network_weights_input[n][x] = (float)rand() / RAND_MAX * 0.001;
+            }
     }
     for (int n = 0; n < Number_of_Output_Neurons; n++) {
         for (int i = 0; i < Number_of_Output_Neurons; i++) {
@@ -55,13 +53,13 @@ void hidden_layer(int layer, int neuron_number, double activated_neurons[Number_
     if (!layer) {
         for (int x = 0; x < 8; x++) {
             for (int y = 0; y < 8; y++) {
-                    sum += temp_board[x][y] * network_weights_input[layer][neuron_number][(x * 8) + y];
+                    sum += temp_board[x][y] * network_weights_input[neuron_number][(x * 8) + y];
             }
         }
-        sum += player * network_weights_input[layer][neuron_number][64];
+        sum += player * network_weights_input[neuron_number][64];
     } else {
         for (int n = 0; n < Number_of_Hidden_Neurons; n++) {
-            sum += activated_neurons[layer-1][n] * network_weights_input[layer][neuron_number][n];
+            sum += activated_neurons[layer-1][n] * network_weights_input[neuron_number][n];
         }
     }
     sum += threshold[layer][neuron_number];
