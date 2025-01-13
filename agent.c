fcce77e4d92c33f5bc3ddaddc09d23a32edfe97e
CitymineHD <lucas.pfeffer@gmx.de>
2025-01-13 09:59:25 +0100

    changed network array

--- agent.c
+++ agent.c
@@ -1,11 +1,73 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-#include "antichess.h"
+//#include "antichess.h"
 #include "agent.h"
 
 char *filename = ".network";
 
-void get_position(char board[8][8], int player, double output[4096]) {  
-    input_layer(board, player, output); // move is a integer in range of 1...4096
+// void get_position(char board[8][8], int player, double output[4096]) {  
+//     input_layer(board, player, output); // move is a integer in range of 1...4096
+// }
+
+void initializeBoard(char board[8][8]){
+    //empty board
+    for (int i = 0; i < 8; i++){
+        for (int j = 0; j < 8; j++){
+            board[i][j] = ' ';
+        }
+    }
+    //pawns
+    for (int i = 0; i < 8; i++){
+        board[1][i] = 'p';
+        board[6][i] = 'P';
+    }
+    //other stuff without loop, kinda ugly but has to be done
+    board[0][0] = 'r';
+    board[0][1] = 'n';
+    board[0][2] = 'b';
+    board[0][3] = 'q';
+    board[0][4] = 'k';
+    board[0][5] = 'b';
+    board[0][6] = 'n';
+    board[0][7] = 'r';
+    board[7][0] = 'R';
+    board[7][1] = 'N';
+    board[7][2] = 'B';
+    board[7][3] = 'Q';
+    board[7][4] = 'K';
+    board[7][5] = 'B';
+    board[7][6] = 'N';
+    board[7][7] = 'R';
+}
+
+void printBoardToTerminal(char board[8][8]){
+    printf("---------------------------------\n");
+    for (int i = 0; i < 8; i++){
+        printf("|");
+        for (int j = 0; j < 8; j++){
+            printf(" %c |", board[i][j]);
+        }
+        printf("\n---------------------------------\n");
+    }
+}
+
+int main() {
+    char board[8][8];
+    double output[4096];
+    initial_network_weights();
+    initializeBoard(board);
+    input_layer(board, 1, output);
+
+    int max_position = -1;
+    double max_value = 0;
+
+    for (int i = 0; i < 4096; i++) {
+        if (output[i] > max_value) {
+            max_position = i;
+            max_value = output[i];
+        }
+    }
+
+    printf("%d: %f", max_position, max_value);
 }
--- network.c
+++ network.c
@@ -9,17 +9,29 @@
 #define Number_of_Hidden_Neurons 2747
 #define Number_of_Output_Neurons 4096
 
-#define Number_of_Layer 5 //Input Layer + 3 Hidden Layer + Output Layer
+#define Number_of_Layer 3 //Input Layer + Hidden Layer + Output Layer
 
-double network_weights[Number_of_Layer-1][Number_of_Output_Neurons];
+double network_weights_input[Number_of_Layer-2][Number_of_Hidden_Neurons][Number_of_Hidden_Neurons];
+double network_weights_output[Number_of_Output_Neurons][Number_of_Output_Neurons];
 double threshold[Number_of_Layer-1][Number_of_Output_Neurons];
 
 void initial_network_weights() {
     srand((unsigned int)time(NULL));
+    for (int i = 0; i < (Number_of_Layer-1); i++) {
+        for (int n = 0; n < Number_of_Hidden_Neurons; n++) {
+                for (int x = 0; x < Number_of_Hidden_Neurons; x++) {
+                    network_weights_input[i][n][x] = (float)rand() / RAND_MAX * 0.001;
+                }
+        }
+    }
+    for (int n = 0; n < Number_of_Output_Neurons; n++) {
+        for (int i = 0; i < Number_of_Output_Neurons; i++) {
+            network_weights_output[n][i] = (float)rand() / RAND_MAX * 0.001;
+        }
+    }
     for (int n = 0; n < (Number_of_Layer-1); n++) {
         for (int i = 0; i < Number_of_Output_Neurons; i++) {
-            network_weights[n][i] = (float)rand() / RAND_MAX * 0.001;
-            threshold[n][i] = (float)rand() / RAND_MAX;
+            threshold[n][i] = 0.5;
         }
     }
 }
@@ -28,28 +40,28 @@ double sigmoid(double x) {
     return 1 / (1 + exp(-x));
 }
 
-void output_layer(int layer, int neuron_number, double activated_neurons[Number_of_Layer][Number_of_Output_Neurons], int player) {
+void output_layer(int layer, int neuron_number, double activated_neurons[Number_of_Layer-1][Number_of_Output_Neurons], int player) {
     double sum = 0;
     for (int n = 0; n < Number_of_Output_Neurons; n++) {
-        sum += activated_neurons[layer-2][n] * network_weights[layer-2][n];
+        sum += activated_neurons[layer-2][n] * network_weights_output[neuron_number][n];
     }
     sum += threshold[layer-2][neuron_number];
 
     activated_neurons[layer-1][neuron_number] = sigmoid(sum);
 }
 
-void hidden_layer(int layer, int neuron_number, double activated_neurons[Number_of_Layer][Number_of_Output_Neurons], int temp_board[8][8], int player) {
+void hidden_layer(int layer, int neuron_number, double activated_neurons[Number_of_Layer-1][Number_of_Output_Neurons], int temp_board[8][8], int player) {
     double sum = 0;
     if (!layer) {
         for (int x = 0; x < 8; x++) {
             for (int y = 0; y < 8; y++) {
-                    sum += temp_board[x][y] * network_weights[layer][(x * 8) + y];
+                    sum += temp_board[x][y] * network_weights_input[layer][neuron_number][(x * 8) + y];
             }
         }
-        sum += player * network_weights[layer][64];
+        sum += player * network_weights_input[layer][neuron_number][64];
     } else {
         for (int n = 0; n < Number_of_Hidden_Neurons; n++) {
-            sum += activated_neurons[layer-1][n] * network_weights[layer][n];
+            sum += activated_neurons[layer-1][n] * network_weights_input[layer][neuron_number][n];
         }
     }
     sum += threshold[layer][neuron_number];
@@ -65,21 +77,21 @@ void input_layer(char board[8][8], int player, double *output) {
         }
     }
 
-    double activated_neurons[Number_of_Layer][Number_of_Output_Neurons];
+    double activated_neurons[Number_of_Layer-1][Number_of_Output_Neurons];
 
-    for (int l = 0; l < Number_of_Layer; l++) {
+    for (int l = 0; l < (Number_of_Layer-1); l++) {
         if (l != (Number_of_Layer-1)) {
             for (int n = 0; n < Number_of_Hidden_Neurons; n++) {
                 hidden_layer(l, n, activated_neurons, temp_board, player);
             }
         } else {
             for (int n = 0; n < Number_of_Output_Neurons; n++) {
-                output_layer(Number_of_Layer, n, activated_neurons, player);
+                output_layer((Number_of_Layer-1), n, activated_neurons, player);
             }
         }
     }
 
     for (int n = 0; n < Number_of_Output_Neurons; n++) {
-        output[n] = activated_neurons[Number_of_Layer-1][n];
+        output[n] = activated_neurons[Number_of_Layer-2][n];
     }
 }
