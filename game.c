65052638a9f3b365a94325a4d159d6bd9d1ed579
Flo <grind@DESKTOP-3M5OGTG.>
2025-01-14 11:51:08 +0100

    added stuff to check into test main hasnt worked so far tho

--- game.c
+++ game.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
 
 #include "antichess.h"
 #include "agent.h"
@@ -15,7 +17,8 @@ int main(void){
     double activated_neurons[Number_of_Layer-1][Number_of_Output_Neurons];
 
     bool legal_moves[Number_of_Output_Neurons];
-
+    srand((unsigned int)time(NULL));
+    int weightToCheck = rand()%Number_of_Hidden_Neurons;
     double deltaInputWeights[Number_of_Hidden_Neurons][Number_of_Input_Neurons];    //array for the summed up gradients for weights between input and hidden layer and hidden and output layer (respectively), needs to be multiplied by eta and then added to the actual network
     double deltaOutputWeights[Number_of_Output_Neurons][Number_of_Hidden_Neurons];  //order is [intoNeuronNumber][outOfNeuronNumber]
     double deltaThresholds[Number_of_Layer-1][Number_of_Output_Neurons];            //same, but for biases, [layerNumber][neuronNumber], layer 0 is first hidden layer
@@ -23,11 +26,11 @@ int main(void){
     initial_network_weights(network_weights_input, network_weights_output, threshold);
     run_network(board, player, network_weights_input, network_weights_output, threshold, activated_neurons);
     determineLegalMoves(legal_moves, player, board);
-    printf("this is test. pls take a moment to pray this shit works.\n neuron for e2e4: %f, is it legal? %d\n", activated_neurons[1][796], legal_moves[796]);
-    backpropStep(board, player, 796, 1, network_weights_input, network_weights_output, threshold, activated_neurons, legal_moves, deltaInputWeights, deltaOutputWeights, deltaThresholds);
+    printf("this is test. pls take a moment to pray this shit works.\n                       neuron for e2e4: %f, and bias is %.20f and weight %d is %.20f\n", activated_neurons[1][796], threshold[1][796], weightToCheck, network_weights_output[796][weightToCheck]);
+    backpropStep(board, player, 796, player, network_weights_input, network_weights_output, threshold, activated_neurons, legal_moves, deltaInputWeights, deltaOutputWeights, deltaThresholds);
     applyDeltas(network_weights_input, network_weights_output, threshold, deltaInputWeights, deltaOutputWeights, deltaThresholds, 0.0001);
     run_network(board, player, network_weights_input, network_weights_output, threshold, activated_neurons);
-    printf("after single backprop step, neuron fires at %f (pls be more than before plspls)\n", activated_neurons[1][796]);
+    printf("after single backprop step, neuron fires at %f, bias is %0.20f and weight %d is %.20f\n", activated_neurons[1][796], threshold[1][796], weightToCheck, network_weights_output[796][weightToCheck]);
 
     return 0;
 }
